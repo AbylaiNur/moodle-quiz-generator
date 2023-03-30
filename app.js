c00672aae042068018f4948cf3bbca719500d64f
AbylaiNur <abylai.nurskenev@gmail.com>
2023-03-31 01:09:56 +0600

    freezed fixing history and settings, therefore I commented them

--- app.js
+++ app.js
@@ -1,67 +1,76 @@
 import express from "express"
+
 const app = express()
-import { getQuizLimiter } from "./middleware/rateLimiter.js";
+import {getQuizLimiter} from "./middleware/rateLimiter.js";
 import expressSession from "express-session"
 import passport from "passport"
-
+import {History} from "./db/models/history.js"
 import "./helpers/auth.js";
-import { google_callback, auth_logout, failure } from "./controllers/authController.js";
+import {google_callback, auth_logout, failure} from "./controllers/authController.js";
 import {
-    context_view,
     download_quiz,
-    history_view,
-    home_view,
-    quiz_context_view,
-    quiz_topic_view,
-} from "./controllers/quizController.js"
+    index_context,
+    index_home,
+    index_quiz_context,
+    // index_quiz_history,
+    index_quiz_topic,
+    index_topic,
+} from "./controllers/controller.js"
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded( { extended: true} ));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(expressSession({
-  resave: false,
-  saveUninitialized: true,
-  secret: process.env.SECRET 
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SECRET
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
- 
+
 function isLoggedIn(req, res, next) {
     req.user ? next() : res.redirect('/auth/google');
 }
 
-app.get('/auth/google', 
-    passport.authenticate('google', { scope: [ 'email', 'profile' ] })
-);
- 
-app.get('/auth/google/callback', 
-    passport.authenticate('google', { failureRedirect: '/failure' }),
-    google_callback
-);
+
+app.get('/', index_home);
+
+app.get('/context', isLoggedIn, index_context)
+
+app.get('/topic', isLoggedIn, index_topic)
+
+app.get('/quiz/topic', isLoggedIn, index_quiz_topic)
+
+app.get('/quiz/context', isLoggedIn, index_quiz_context)
+
+// app.get('/history/:id', isLoggedIn, index_quiz_history)
+
+app.post('/download', isLoggedIn, download_quiz)
+
+// app.get('/settings', isLoggedIn, (req, res) => {
+//     res.render('settings', {user: req.user});
+// })
 
 app.get('/logout', auth_logout);
 
 app.get('/failure', failure);
 
-app.get('/', home_view);
+app.get('/auth/google',
+    passport.authenticate('google', {scope: ['email', 'profile']})
+);
 
-app.get('/context', isLoggedIn, context_view)
+app.get('/auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/failure'}),
+    google_callback
+);
 
-app.get('/topic', isLoggedIn, )
-
-app.get('/quiz/topic', isLoggedIn, quiz_topic_view)
-
-app.get('/quiz/context', isLoggedIn, quiz_context_view)
-
-app.get('/history', isLoggedIn, history_view)
-
-app.post('/download', download_quiz)
-
-app.get('/settings', isLoggedIn ,(req, res) => {
-    res.render('settings', { user : req.user });
+app.listen(process.env.PORT || 3000, () => {
+    const history = new History;
+    history.createTables().catch((res) => {
+        console.error(res)
+    });
 })
 
-app.listen(process.env.PORT || 3000)
 
--- db/models/user.js
+++ db/models/user.js
@@ -1,26 +1,30 @@
 import {pool} from "../connection.js"
 
-class User{
-    getUsers () {
+class User {
+    getUsers() {
         return pool.query("SELECT firstname, email from users");
     }
-    findUserById(param){
+
+    findUserById(param) {
         return pool.query("SELECT id, firstname, email from users WHERE id=$1",
             param);
     }
-    findUserByEmail(param){
+
+    findUserByEmail(param) {
         return pool.query("SELECT id, firstname, email from users WHERE email=$1",
             param);
     }
-    createNewUser(param){
-        return pool.query("INSERT INTO users(firstname, email) VALUES($1, $2) RETURNING id, firstname, email", 
+
+    createNewUser(param) {
+        return pool.query("INSERT INTO users(firstname, email) VALUES($1, $2) RETURNING id, firstname, email",
             param);
     }
-    saveApiKey(param){
+
+    saveApiKey(param) {
         return pool.query("INSERT INTO ")
     }
 }
 
-export{
+export {
     User
 }
